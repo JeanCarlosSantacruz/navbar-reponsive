9ef5c0551b58c05bfec3508bb9cdbf69931e8bda
Jean Carlos Santacruz <jeancarlos@javerianacali.edu.co>
2022-10-23 18:38:41 -0500

    Login completo

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import App from './App';
 import About from './components/About';
 import QuienesSomos from './components/QuienesSomos';
 import Ligas from './components/Ligas/Ligas';
+import IniciarSesion from './components/IniciarSesion/IniciarSesion';
+import Selecciones from './components/Selecciones/Selecciones';
+import Torneos from './components/Torneos/Torneos';
 import {Routes, Route, BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,6 +19,10 @@ root.render(
         <Route path="/About" element={<About />}/>
         <Route path="/QuienesSomos" element={<QuienesSomos/>}/>
         <Route path="/Ligas" element={<Ligas/>}/>
+        <Route path="/Torneos" element={<Torneos/>}/>
+        <Route path="/Selecciones" element={<Selecciones/>}/>
+        <Route path="/IniciarSesion" element={<IniciarSesion/>}/>
+        
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
